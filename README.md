004d2b0c2f148344c5799510624543b21cda38e0
duonghb53 <duonghb53@gmail.com>
2023-07-17 14:07:49 +0700

    impl testing

---
+++ README.md
@@ -0,0 +1,12 @@
+CosmWasm Academy
+================================================================
+Master CosmWasm and build your secure, multi-chain dApp on any CosmWasm chain!
+1. Boost your Rust, CosmWasm, and CosmJS Skill
+2. Develop your own DApp
+3. Enhance career development
+4. Strengthen connections with other committers and developers
+5. Gain the certificates of achievement issued by Confio, door to open job pool for all these chains
+
+
+### Smartcontracts
+1. [Counting Contractors](/counting_contract/)
--- counting_contract/src/lib.rs
+++ counting_contract/src/lib.rs
@@ -29,3 +29,44 @@ pub fn query(_deps: Deps, _env: Env, _msg: msg::QueryMsg) -> StdResult<Binary> {
         Value { value } => to_binary(&query::value(value + 1)),
     }
 }
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        execute, instantiate,
+        msg::{QueryMsg, ValueResponse},
+        query,
+    };
+    use cosmwasm_std::{Addr, Empty};
+    use cw_multi_test::{App, Contract, ContractWrapper, Executor};
+
+    fn counting_contract() -> Box<dyn Contract<Empty>> {
+        let contract = ContractWrapper::new(execute, instantiate, query);
+        Box::new(contract)
+    }
+
+    #[test]
+    fn query_value() {
+        let mut app = App::default();
+
+        let contract_id = app.store_code(counting_contract());
+
+        let contract_addr = app
+            .instantiate_contract(
+                contract_id,
+                Addr::unchecked("sender"),
+                &Empty {},
+                &[],
+                "Counting Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: ValueResponse = app
+            .wrap()
+            .query_wasm_smart(contract_addr, &QueryMsg::Value { value: 1 })
+            .unwrap();
+
+        assert_eq!(resp, ValueResponse { value: 2 });
+    }
+}
